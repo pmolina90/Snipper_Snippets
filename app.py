745f18d6786a38fc90940be242307ae95f51625b
pmolina90 <P.a.molina90@gmail.com>
2024-04-24 17:03:40 -0400

    starting from scratch again, reattempting encryption

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 from flask import Flask, jsonify, request
 import json
-from encryption_utils import read_symmetric_key, encrypt_data, decrypt_data
 
 app = Flask(__name__)
 
-# Load seed data
+# Load seed data from seedData.json file
 with open('seedData.json', 'r') as f:
     seed_data = json.load(f)
 
-# Read symmetric encryption key from file
-symmetric_key = read_symmetric_key('./symmetric_key.txt')
-
+# Route to add a new snippet
 @app.route('/snippets', methods=['POST'])
 def add_snippet():
     snippet_data = request.json
-    print("Original code:", snippet_data['code'])  # Print original code
-    # Encrypt the code content before saving
-    encrypted_code = encrypt_data(snippet_data['code'], symmetric_key)
-    print("Encrypted code:", encrypted_code)  # Print encrypted code
-    snippet_data['code'] = encrypted_code
     # Assign an ID to the new snippet
     snippet_data['id'] = len(seed_data) + 1
     seed_data.append(snippet_data)
@@ -27,12 +19,28 @@ def add_snippet():
         json.dump(seed_data, f, indent=2)
     return jsonify(snippet_data), 201
 
+# Route to retrieve all snippets *** WITH BONUS ***
 @app.route('/snippets', methods=['GET'])
 def get_snippets():
-    # Decrypt the code content before returning
-    decrypted_data = [{**snippet, 'code': decrypt_data(snippet['code'], symmetric_key)} for snippet in seed_data]
-    print("Decrypted data:", decrypted_data)  # Print decrypted data
-    return jsonify(decrypted_data)
+    lang = request.args.get('lang')  # Get language parameter from the request
+    print("Language:", lang)  # Debugging print statement
+    if lang:
+        # Filter snippets based on the language from search
+        filtered_snippets = [snippet for snippet in seed_data if snippet.get('language', '').lower() == lang.lower()]
+        print("Filtered snippets:", filtered_snippets)  # Debugging print statement
+        return jsonify(filtered_snippets)
+    else:
+        # If no language param provided, return ALL
+        return jsonify(seed_data)
+
+# Route to retrieve a snippet by its ID
+@app.route('/snippets/<int:id>', methods=['GET'])
+def get_snippet_by_id(id):
+    snippet = next((s for s in seed_data if s['id'] == id), None)
+    if snippet:
+        return jsonify(snippet)
+    else:
+        return jsonify({'error': 'Snippet not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
